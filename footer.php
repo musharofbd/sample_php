34320bbc90479a73b121432a1dc8beddf9b9a264
Musharof Hossain <musharofwebdev@gmail.com>
2020-04-10 16:39:03 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<?php 
+
+
+echo "<p>Copy right &copy; 1995-".date("Y")." www.google.com </p>";
+
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Page title</title>
+	</head>
+	<body>
+
+		<?php
+		
+		echo "Hello, world! <br />"; 
+		
+		$capital = 50;
+		echo "Variable capital is $capital <br>";
+		echo "Variable capital is $Capital <br>";
+      
+		
+		
+		
+		
+		?>
+
+
+
+	</body>
+
+</html>
